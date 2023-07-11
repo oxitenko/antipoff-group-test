6798fe8bf556f90e7b25bb89b5e2551214ddb825
Oksana Komarova <rabota_komarova@mail.ru>
2023-07-11 22:17:00 +0700

    finish main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,26 +2,47 @@ import "./Main.css"
 import LogoutButton from "../LogoutButton/LogoutButton";
 import {useSelector} from "react-redux";
 import UserCard from "../UserCard/UserCard";
+import arrowDown from "../../images/arrowDown.svg"
+import {useEffect, useState} from "react";
+import useResize from "../../hooks/useResize";
 
 const Main = () => {
 
+    const [pagination, setPagination] = useState(0);
     const users = useSelector(state => state.users?.users);
+    const windowSize = useResize();
+    const loadMore = () => {
+        setPagination(state => state + 4)
+    }
+
+    useEffect(() => {
+        if (windowSize.width >= 860) {
+            setPagination(8)
+        } else {
+            setPagination(4)
+        }
+    },[windowSize.width]);
 
     return (<>
-            <div className="team">
-                <LogoutButton/>
-                <div className="team_container">
-                    <h1 className="team_title">Наша команда</h1>
-                    <p className="team_text">Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые
-                        ложатся на их плечи, и умеющие
-                        находить выход из любых, даже самых сложных ситуаций.</p>
-                </div>
+        <div className="team">
+            <LogoutButton/>
+            <div className="team_container">
+                <h1 className="team_title">Наша команда</h1>
+                <p className="team_text">Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые
+                    ложатся на их плечи, и умеющие
+                    находить выход из любых, даже самых сложных ситуаций.</p>
             </div>
-            <ul className="team_list">
-                {users.map(user => <UserCard user={user} key={user.id}/>)}
-            </ul>
-        </>
-    );
+        </div>
+        <ul className="team_list">
+            {users.slice(0, pagination).map(user => <UserCard user={user} key={user.id}/>)}
+        </ul>
+        {pagination !== users.length ? <button
+            onClick={loadMore}
+            className="team_loadmore"
+            type="button">Показать еще <img src={arrowDown} alt="arrow"/>
+        </button> : null}
+
+    </>);
 };
 
 export default Main;
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -3,12 +3,12 @@ import like from "../../images/heartIcon.svg"
 const UserCard = ({user}) => {
     return (
         <li className="user-card">
-            <div>
-                <img src={user.avatar} alt={user.first_name}/>
-                <p>{user.first_name} {user.last_name}</p>
+            <div className="user-card_info">
+                <img className="user-card_avatar" src={user.avatar} alt={user.first_name}/>
+                <p className="user-card_name">{user.first_name} {user.last_name}</p>
             </div>
-            <div>
-                <button><img src={like} alt="like"/></button>
+            <div className="user-card_likewrap">
+                <button className="user-card_likebtn" type="button"><img src={like} alt="like"/></button>
             </div>
         </li>
     );
